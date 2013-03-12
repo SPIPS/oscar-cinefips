6e6fc7b8832d28a82c050f34df98840bd9a4fd13
Romain Beuque <rbeuque74@gmail.com>
2013-03-12 13:04:43 +0100

    welcome_cat favicon

--- application/views/welcome_cat.php
+++ application/views/welcome_cat.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
+	<link rel="shortcut icon" href="favicon.ico">
 
 	<!-- Le styles -->
 	<link href="<?php echo base_url(); ?>css/bootstrap.min.css" rel="stylesheet" media="screen"/>
